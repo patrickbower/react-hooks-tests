47a2415c304bc71122f4fe15132886b84859ebf2
Patrick Bower <bower.patrick@gmail.com>
2019-12-21 14:53:01 +0000

    started

--- src/Character.js
+++ src/Character.js
@@ -1,37 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {GoGet, urlAll} from './Ajax';
 import {Link} from 'react-router-dom';
 
-class Characters extends React.Component {
-  constructor(){
-    super();
-    
-    this.state = {
-      list: []
-    }
-  }
+const Characters = () => {
 
-  componentDidMount() {
-    GoGet(urlAll())
-      .then(data => {
-        this.setState({
-          list: data.results
-        })
-      });
-  }
+  // const state = {
+  //   list: []
+  // }
+
+  const [list, setList] = useState([]);
   
-  render() {
-    return (
-      this.state.list.map(each => {
-        return (
-          <Link key={each.id} to={`/${each.id}`}>
-            <h2>{each.name}</h2>
-            <img src={each.image} alt=""/>
-          </Link>
-        )
-      })
-    )
+  // componentDidMount() {
+    //   GoGet(urlAll())
+    //     .then(data => {
+      //       this.setState({
+        //         list: data.results
+        //       })
+        //     });
+        // }
+  
+
+  const getAllCharacters = () => {
+    return GoGet(urlAll())
+      .then(data => {
+          return data.results
+        });
   }
+        
+  useEffect(() => {
+    getAllCharacters().then(data => { console.log(data) });
+  },[]);
+
+
+  return (
+    list.map(each => {
+      return (
+        <Link key={each.id} to={`/${each.id}`}>
+          <h2>{each.name}</h2>
+          <img src={each.image} alt=""/>
+        </Link>
+      )
+    })
+  )
 }
 
 export default Characters;
